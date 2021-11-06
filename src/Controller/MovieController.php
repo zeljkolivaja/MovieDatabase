d8833472982184d0f0b5c21680f781f30ee228bf
Zeljko Livaja <zeljaos@gmail.com>
2021-11-06 18:56:54 +0100

    installed twig

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MovieController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="app_homepage")
+     */
+    public function homepage(MovieRepository $repository)
+    {
+
+
+
+        return $this->render('movie/homepage.html.twig', [
+            "movies" => $movies
+        ]);
+    }
+}
